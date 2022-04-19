8a23fbaaa5ab7434b0bd2e83b603296509b7d939
Wilson Chen <wilson0109@outlook.com>
2022-04-19 11:17:47 -0400

    updated DecisionTrees.py

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -1,4 +1,61 @@
-with open('mushroom.csv', 'r') as f:
-    lines = [line.strip().split(',') for line in f.readlines()]
-    lines = lines[1:]
+import math
 
+with open('play_tennis.csv', 'r') as f:
+    lines = [line.strip().split(',') for line in f.readlines()]
+    # make a dictionary with key as first row and value as column
+    header = lines[0]
+    data = lines[1:]
+
+def calc_starting_entropy(characteristic):
+    if type(characteristic) != int:
+        characteristic = header.index(characteristic)
+    counts = {}
+    for row in data:
+        value = row[characteristic]
+        if value not in counts:
+            counts[value] = 0
+        counts[value] += 1    
+    entropy = 0
+    for value in counts:
+        p = counts[value] / len(data)
+        entropy -= p * math.log2(p)
+    return entropy
+
+def calc_entropy(characteristic, cl):
+    if type(characteristic) != int:
+        characteristic = header.index(characteristic)
+    counts = {}
+    for row in cl:
+        value = row[characteristic]
+        if value not in counts:
+            counts[value] = 0
+        counts[value] += 1    
+    entropy = 0
+    for value in counts:
+        p = counts[value] / len(cl)
+        entropy -= p * math.log2(p)
+    return entropy
+
+def calc_expected_entropy(characteristic):
+    characteristic = header.index(characteristic)
+    counts = {}
+    newRows = {}
+    for row in data:
+        value = row[characteristic]
+        if value not in counts:
+            counts[value] = 0
+            newRows[value] = []
+        counts[value] += 1
+        newRows[value].append(row)
+    expected_entropy = 0
+    for value in counts:
+        p = counts[value] / len(data)
+        expected_entropy += p * calc_entropy(-1, newRows[value])
+    return expected_entropy
+
+def calc_information_gain(characteristic):
+    return calc_starting_entropy(-1) - calc_expected_entropy(characteristic)
+
+print(calc_information_gain('Outlook'))
+print(calc_information_gain('Humidity'))
+print(calc_information_gain('Wind'))
